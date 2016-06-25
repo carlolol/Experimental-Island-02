e2e879aa8733914d3a926eee1c64fe39f01229d1
Carl Louie Aruta <arutacarllouie@yahoo.com>
2016-06-25 15:25:23 +0800

    With Character

---
+++ Assets/Models/Character/Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+    private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("click");
+            anim.SetBool("attack", true);
+        }
+        else if(Input.GetMouseButtonUp(0))
+            anim.SetBool("attack", false);
+	}
+}
